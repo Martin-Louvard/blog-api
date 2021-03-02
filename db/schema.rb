b2895c0f1c666f7582d2cec69f5cdcf10ea40e6b
Martin-Louvard <martin.louvard@gmail.com>
2021-03-02 20:00:32 +0100

    third commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_162726) do
+ActiveRecord::Schema.define(version: 2021_03_02_185951) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_162726) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_private", default: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
